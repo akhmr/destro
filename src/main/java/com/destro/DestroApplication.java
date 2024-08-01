6b902cb6c66894fb9711e148f0b3f55f290f625b
arvind7600 <arvind@bluesheets.io>
2024-08-01 11:59:08 +0530

    init repo

---
+++ src/main/java/com/destro/DestroApplication.java
@@ -0,0 +1,13 @@
+package com.destro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/destro/DestroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
