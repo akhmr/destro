67bed8c3c4a78e38c5695d78044e152128144429
arvind7600 <arvind@bluesheets.io>
2024-08-01 18:33:33 +0530

    adding data source and db configuration

---
+++ src/main/java/com/destro/config/DbConfig.java
@@ -0,0 +1,64 @@
+package com.destro.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.persistence.EntityManagerFactory;
+
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
+public class DbConfig {
+
+    @Autowired
+    private Environment env;
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		return dataSourceBuilder
+				.driverClassName(env.getProperty("jdbc.driverClassName"))
+				.url(env.getProperty("jdbc.url"))
+				.username(env.getProperty("jdbc.username"))
+				.password(env.getProperty("jdbc.password"))
+				.build();
+
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		System.out.println("creating em");
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource);
+		em.setPackagesToScan("com.destro.repo");
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		return em;
+	}
+
+	
+	 @Bean
+	  public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+	    JpaTransactionManager txManager = new JpaTransactionManager();
+	    txManager.setEntityManagerFactory(entityManagerFactory);
+	    return txManager;
+	  }
+
+}
